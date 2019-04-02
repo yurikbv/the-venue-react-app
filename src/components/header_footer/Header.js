c1c75d68f19e0429ef280c001b430c397dc5ad8e
yurikbv <yurikbvv@gmail.com>
2019-04-03 01:12:31 +0300

    first commit

---
+++ src/components/header_footer/Header.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+import SideDrawer from "./SideDrawer";
+
+class Header extends Component {
+
+  state = {
+    drawerOpen: false
+  };
+
+  toggleDrawer = value => {
+    this.setState({
+      drawerOpen: value
+    })
+  };
+
+  render() {
+
+    const {drawerOpen} = this.state;
+
+    return (
+      <AppBar
+        position="fixed"
+        style={{
+          background: '#2f2f2f',
+          boxShadow: 'none',
+          padding: '10px 0'}}
+      >
+        <Toolbar>
+          <div className="header_logo">
+            <div className="font_righteous header_logo_venue">The Venue</div>
+            <div className="header_logo_title">Musical Events</div>
+          </div>
+          <IconButton
+            aria-label="Menu"
+            color="inherit"
+            onClick={() => this.toggleDrawer(true)}
+          >
+            <MenuIcon/>
+          </IconButton>
+          <SideDrawer
+            open={drawerOpen}
+            onClose={(value) => this.toggleDrawer(value)}
+          />
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+export default Header;
